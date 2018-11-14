ad7034b0dfe74689b1cd8a25128aaa07172d3302
suyichen <805788048@qq.com>
2018-11-14 19:32:29 +0800

    can move

--- app/src/main/java/com/syc/smileyface/MainActivity.kt
+++ app/src/main/java/com/syc/smileyface/MainActivity.kt
@@ -2,12 +2,21 @@ package com.syc.smileyface
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val face = findViewById<SmileyFace>(R.id.smileyFace)
+        face.setImage(R.drawable.skin_tab_icon_conversation_normal, R.drawable.skin_tab_icon_conversation_selected, R.drawable.rvq, R.drawable.rvr)
+        face.setOnMenuClickListener(object : SmileyFace.OnMenuClickListener {
+            override fun onItemClick(view: View) {
+                Toast.makeText(this@MainActivity, "Click " + face.isHasClick(), Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
 }
--- app/src/main/java/com/syc/smileyface/SmileyFace.kt
+++ app/src/main/java/com/syc/smileyface/SmileyFace.kt
@@ -22,7 +22,7 @@ class SmileyFace : FrameLayout {
     private var childView2Y: Float = 0.toFloat()
     private var centerX: Float = 0.toFloat()
     private var centerY: Float = 0.toFloat()
-    private val hasClick: Boolean = false
+    private var hasClick: Boolean = false
     private var normal1: Int? = null
     private var click1: Int? = null
     private var normal2: Int? = null
@@ -49,12 +49,67 @@ class SmileyFace : FrameLayout {
     }
 
 
-    fun setOnMenuClickListener(onMenuClickListener: OnMenuClickListener){
+    fun setOnMenuClickListener(onMenuClickListener: OnMenuClickListener) {
         this.onMenuClickListener = onMenuClickListener
+    }
+
+    fun setImage(normal1: Int, click1: Int, normal2: Int, click2: Int) {
+        this.normal1 = normal1
+        this.click1 = click1
+        this.normal2 = normal2
+        this.click2 = click2
+        refreshDrawable()
+    }
+
+    fun setHasClick(hasClick: Boolean){
+        this.hasClick = hasClick
+        refreshDrawable()
+    }
+
+    private fun refreshDrawable() {
+        if (hasClick) {
+            if (click1 != 0)
+                click1?.let { childView1?.setBackgroundResource(it) }
+            if (click2 != 0)
+                click2?.let { childView2?.setBackgroundResource(it) }
+        } else {
+            if (normal1 != 0)
+                normal1?.let { childView1?.setBackgroundResource(it) }
+            if (normal2 != 0)
+                normal2?.let { childView2?.setBackgroundResource(it) }
+        }
     }
 
     interface OnMenuClickListener {
         fun onItemClick(view: View)
+    }
+
+    private fun isContain(view: View, rawX: Float, rawY: Float): Boolean {
+        val point = IntArray(2)
+        view.getLocationOnScreen(point)
+        return x >= point[0] && x <= point[0] + view.width && y >= point[1] && y <= point[1] + view.height
+    }
+
+    private fun changeWhenMove(x: Float, y: Float) {
+        var x = x
+        var y = y
+        if (y + centerY < -12 * centerY) {
+            y = -12 * centerY - centerY
+        } else if (y - centerY > 12 * centerY) {
+            y = 12 * centerY + centerY
+        }
+        if (x + centerX < -12 * centerX) {
+            x = -12 * centerX - centerX
+        } else if (x - centerX > 12 * centerX) {
+            x = 12 * centerX + centerX
+        }
+        childView1?.x = childView1X + (x - centerX) / 23
+        childView1?.y = childView1Y + (y - centerY) / 23
+        if (childView2 != null) {
+            childView2!!.x = childView2X + (x - centerX) / 10
+            childView2!!.y = childView2Y + (y - centerY) / 10
+        }
+
     }
 
     override fun onWindowFocusChanged(hasWindowFocus: Boolean) {
@@ -73,11 +128,38 @@ class SmileyFace : FrameLayout {
         childView2Y = childView2?.y!!
     }
 
+    private fun restorePosition() {
+        childView1?.x = childView1X
+        childView1?.y = childView1Y
+        childView2?.x = childView2X
+        childView2?.y = childView2Y
+    }
+
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
-        return super.onInterceptTouchEvent(ev)
+        return true
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return super.onTouchEvent(event)
+        val x = event?.x
+        val y = event?.y
+        val action = event?.action
+        when (action) {
+            MotionEvent.ACTION_MOVE -> {
+                changeWhenMove(x!!, y!!)
+                return true
+            }
+            MotionEvent.ACTION_UP -> {
+                restorePosition()
+                if (isContain(this, event.rawX, event.rawY)) {
+                    setHasClick(!hasClick)
+                    if (onMenuClickListener != null) {
+                        onMenuClickListener!!.onItemClick(this)
+                    }
+                }
+                return true
+            }
+        }
+        return true
     }
+
 }
